631c41e772f545e94015f8ef9691fe8df6422c89
Curtis Rueden <ctrueden@wisc.edu>
2011-04-18 12:15:24 -0500

    Add script for automating creation of symlinks.

---
+++ config-links.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+set -e
+
+CONFIG_DIR=`cd "$(dirname $0)"; pwd`
+cd
+USER_DIR=`pwd`
+LINK_DIR=${CONFIG_DIR#$USER_DIR/}
+
+OS_NAME=`uname`
+STAMP=`date +%Y%m%dT%H%M%S`
+
+FILES=(
+.ant-global.properties
+.gitconfig
+.keystore
+.plan
+.profile
+.vim
+.vimrc
+.ssh/config
+Library/KeyBindings
+)
+
+for f in ${FILES[@]}
+do
+  if [ -h $f ]; then
+    (set -x; rm $f)
+  fi
+  if [ -a $f ]; then
+    bk=$f.$STAMP
+    (set -x; mv $f $bk)
+  fi
+done
+
+(
+  set -x
+  ln -s $LINK_DIR/ant-global.properties .ant-global.properties
+  ln -s $LINK_DIR/gitconfig .gitconfig
+  ln -s $LINK_DIR/restless.keystore .keystore
+  ln -s $LINK_DIR/plan .plan
+  ln -s $LINK_DIR/vim .vim
+  ln -s $LINK_DIR/vimrc .vimrc
+)
+
+if [ "$OS_NAME" == "Linux" ]; then
+  (set -x; ln -s $LINK_DIR/profile .bashrc)
+else
+  (set -x; ln -s $LINK_DIR/profile .profile)
+fi
+
+cd .ssh
+(set -x; ln -s ../$LINK_DIR/ssh-config config)
+cd ..
+
+if [ "$OS_NAME" == "Darwin" ]; then
+  cd Library
+  (set -x; ln -s ../$LINK_DIR/KeyBindings)
+  cd ..
+fi
+
