6e311511672ddd9101e6e1222ddd43abd9b8151a
Curtis Rueden <ctrueden@wisc.edu>
2015-06-16 12:42:03 -0500

    config-links.sh: improve the code structure
    This splits out common functions into... well... functions.
    And it eliminates the redundant FILES list in favor of simply
    enumerating the files to process with function calls.

--- config-links.sh
+++ config-links.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+# -- Global variables --
+
 CONFIG_DIR=$(cd "$(dirname $0)"; pwd)
 cd
 USER_DIR=$(pwd)
@@ -10,45 +12,57 @@ LINK_DIR=${CONFIG_DIR#$USER_DIR/}
 OS_NAME=$(uname)
 STAMP=$(date +%Y%m%dT%H%M%S)
 
-FILES=(
-.bashrc
-.forward
-.gitconfig
-.mrconfig
-.plan
-.vimrc
-.zshrc
-Library/KeyBindings
-)
+# -- Functions --
 
-for f in ${FILES[@]}
-do
-  if [ -h $f ]; then
-    (set -x; rm $f)
+# Clears away the given file, backing it up first if needed.
+# If it is a real file (not a symlink), it is renamed.
+# Or if the file is a symlink, it is simply deleted.
+clear_file() {
+  f="$1"
+  if [ -h "$f" ]; then
+    (set -x; rm "$f")
   fi
-  if [ -a $f ]; then
-    bk=$f.$STAMP
-    (set -x; mv $f $bk)
+  if [ -a "$f" ]; then
+    bk="$f.$STAMP"
+    (set -x; mv "$f" "$bk")
   fi
-done
+}
 
-(
-  set -x
-  echo "export DOTFILES=\"$CONFIG_DIR\"" > .bashrc
-  echo ". \"$DOTFILES/bashrc\"" >> .bashrc
-  ln -s $LINK_DIR/forward .forward
-  ln -s $LINK_DIR/gitconfig .gitconfig
-  ln -s $LINK_DIR/mrconfig .mrconfig
-  ln -s $LINK_DIR/plan .plan
-  ln -s $LINK_DIR/vimrc .vimrc
-  echo "export DOTFILES=\"$CONFIG_DIR\"" > .zshrc
-  echo "source \"$DOTFILES/zshrc\"" >> .zshrc
-)
+# Copies the given source to the specified destination.
+# Does nothing if files match; otherwise, replaces the original file.
+install_file() {
+  src="$1"
+  dest="$2"
+  diff "$src" "$dest" > /dev/null 2>&1 ||
+    (clear_file "$dest"; set -x; cp "$src" "$dest")
+}
+
+# Symlinks the given source to the specified destination.
+link_file() {
+  src="$1"
+  dest="$2"
+  clear_file "$dest"
+  (set -x; ln -s "$src" "$dest")
+}
+
+# -- Main --
+
+echo "export DOTFILES=\"$CONFIG_DIR\"" > "$TMPDIR/bashrc.stub"
+echo '. "$DOTFILES/bashrc"' >> "$TMPDIR/bashrc.stub"
+install_file "$TMPDIR/bashrc.stub" .bashrc
+link_file "$LINK_DIR/forward" .forward
+link_file "$LINK_DIR/gitconfig" .gitconfig
+link_file "$LINK_DIR/mrconfig" .mrconfig
+link_file "$LINK_DIR/plan" .plan
+link_file "$LINK_DIR/vimrc" .vimrc
+echo "export DOTFILES=\"$CONFIG_DIR\"" > "$TMPDIR/zshrc.stub"
+echo 'source "$DOTFILES/zshrc"' >> "$TMPDIR/zshrc.stub"
+install_file "$TMPDIR/zshrc.stub" .zshrc
 
 case "$(uname)" in
   Darwin)
     cd Library
-    (set -x; ln -s ../$LINK_DIR/KeyBindings)
+    link_file "../$LINK_DIR/KeyBindings" KeyBindings
     cd ..
     ;;
 esac
