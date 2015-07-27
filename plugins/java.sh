b1002f8754459b51b703dddd984713f8eb9adbb3
Mark Hiner <hinerm@gmail.com>
2015-07-27 14:23:23 -0500

    Fix J6,J7,J8 on linux
    Now calls update-java-alternatives to set the jvm, and sets java home.
    Only supports oracle java at the moment.
    
    Closes #3

--- plugins/java.sh
+++ plugins/java.sh
@@ -4,20 +4,29 @@ if [ -x /usr/libexec/java_home ]; then
 	jhome() {
 		/usr/libexec/java_home -v "$@"
 	}
-elif [ -x /usr/sbin/update-java-alternatives ]; then
+else
+	jhome() { :; }
+fi
+
+if [ -x /usr/sbin/update-java-alternatives ]; then
 	# Linux
 	jhome() {
 		/usr/sbin/update-java-alternatives -l | grep "$@" | head -n 1 | cut -f 3 -d ' '
 	}
+	export J6="$(jhome '6-oracle')"
+	export J7="$(jhome '7-oracle')"
+	export J8="$(jhome '8-oracle')"
+	alias j6='export JAVA_HOME="$J6" && sudo update-java-alternatives --set java-6-oracle && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+	alias j7='export JAVA_HOME="$J7" && sudo update-java-alternatives --set java-7-oracle && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+	alias j8='export JAVA_HOME="$J8" && sudo update-java-alternatives --set java-8-oracle && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 else
-	jhome() { :; }
+	export J6="$(jhome '1.6')"
+	export J7="$(jhome '1.7')"
+	export J8="$(jhome '1.8')"
+	alias j6='export JAVA_HOME="$J6" && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+	alias j7='export JAVA_HOME="$J7" && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+	alias j8='export JAVA_HOME="$J8" && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 fi
-export J6="$(jhome '1.6')"
-export J7="$(jhome '1.7')"
-export J8="$(jhome '1.8')"
-alias j6='export JAVA_HOME="$J6" && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
-alias j7='export JAVA_HOME="$J7" && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
-alias j8='export JAVA_HOME="$J8" && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 
 # unset the actual classpath, since some programs play badly with it
 unset CLASSPATH
