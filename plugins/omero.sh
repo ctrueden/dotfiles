b9568f88fa8aa55357ca6fefa3c449477bc9acb3
Curtis Rueden <ctrueden@wisc.edu>
2017-01-04 10:55:46 -0600

    omero: remove PYTHONPATH
    Apparently, with Ice 3.6, it is no longer needed to set.
    And it definitely hoses up Python 3 on macOS to have it set.

--- plugins/omero.sh
+++ plugins/omero.sh
@@ -11,13 +11,11 @@ case "$(uname)" in
 	Linux)
 		export ICE_HOME=/usr/share/Ice-3.4.2
 		export POSTGRES_HOME=/usr/lib/postgresql/9.1
-		export PYTHONPATH=/usr/lib/pymodules/python2.7:$PYTHONPATH
 		export LD_LIBRARY_PATH=/usr/share/java:/usr/lib:$LD_LIBRARY_PATH
 		;;
 	Darwin)
 		export ICE_HOME="$(brew --prefix ice)"
 		export SLICEPATH="$ICE_HOME/share/Ice-3.5/slice"
-		export PYTHONPATH="$OMERO_PREFIX/lib/python:/usr/local/lib/python2.7/site-packages"
 		;;
 esac
 export PATH="$OMERO_PREFIX/bin:$PATH"
