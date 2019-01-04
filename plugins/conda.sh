4e61f86a89aa2b35f30f4d7e960c0e148046a408
Curtis Rueden <ctrueden@wisc.edu>
2019-01-04 13:04:45 -0600

    Update how conda is configured, for conda 4.5
    Now featuring "conda activate" instead of "source activate".

---
+++ plugins/conda.sh
@@ -0,0 +1,5 @@
+# Enable conda if installed.
+if [ -d /usr/local/miniconda3 ]
+then
+  . /usr/local/miniconda3/etc/profile.d/conda.sh
+fi
--- plugins/zzz_path.sh
+++ plugins/zzz_path.sh
@@ -5,12 +5,6 @@ $CODE_SCIJAVA/scijava-scripts:\
 $CODE_FIJI/bin:\
 $PATH
 
-# prepend Miniconda bin directory to the path, if applicable
-MINICONDA_DIR=/usr/local/miniconda3/bin
-if [ -d "$MINICONDA_DIR" ]; then
-	export PATH="$MINICONDA_DIR:$PATH"
-fi
-
 # prepend Homebrew bin directories to the path, if applicable
 if [ -d "$BREW/sbin" ]; then
 	export PATH="$BREW/sbin:$PATH"
