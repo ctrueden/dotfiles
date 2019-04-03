7fb1a1a71530e3e73b5ab6545d447ae736ca0e4a
Curtis Rueden <ctrueden@wisc.edu>
2019-04-03 15:02:35 -0500

    java: add support for switching to Java 12

--- plugins/java.sh
+++ plugins/java.sh
@@ -23,18 +23,21 @@ export J8="$(jhome '1.8')"
 export J9="$(jhome '9')"
 export J10="$(jhome '10')"
 export J11="$(jhome '11')"
+export J12="$(jhome '12')"
 test -n "$J6" || export J6="$(jhome '6-oracle')"
 test -n "$J7" || export J7="$(jhome '7-oracle')"
 test -n "$J8" || export J8="$(jhome '8-oracle')"
 test -n "$J9" || export J9="$(jhome '9-oracle')"
 test -n "$J10" || export J10="$(jhome '10-oracle')"
 test -n "$J11" || export J11="$(jhome '11-oracle')"
+test -n "$J12" || export J12="$(jhome '12-oracle')"
 alias j6='export JAVA_HOME="$J6" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j7='export JAVA_HOME="$J7" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j8='export JAVA_HOME="$J8" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j9='export JAVA_HOME="$J9" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j10='export JAVA_HOME="$J10" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j11='export JAVA_HOME="$J11" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+alias j12='export JAVA_HOME="$J12" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 
 # use Java 8 by default if available
 test -n "$J8" && export JAVA_HOME="$J8"
