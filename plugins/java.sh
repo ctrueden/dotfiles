a3cab714169cf24e215099146af8dd4059606927
Curtis Rueden <ctrueden@wisc.edu>
2019-09-30 13:38:02 +0200

    Add shortcuts for Java 13 - 19

--- plugins/java.sh
+++ plugins/java.sh
@@ -24,6 +24,13 @@ export J9="$(jhome '9')"
 export J10="$(jhome '10')"
 export J11="$(jhome '11')"
 export J12="$(jhome '12')"
+export J13="$(jhome '13')"
+export J14="$(jhome '14')"
+export J15="$(jhome '15')"
+export J16="$(jhome '16')"
+export J17="$(jhome '17')"
+export J18="$(jhome '18')"
+export J19="$(jhome '19')"
 test -n "$J6" || export J6="$(jhome '6-oracle')"
 test -n "$J7" || export J7="$(jhome '7-oracle')"
 test -n "$J8" || export J8="$(jhome '8-oracle')"
@@ -31,6 +38,13 @@ test -n "$J9" || export J9="$(jhome '9-oracle')"
 test -n "$J10" || export J10="$(jhome '10-oracle')"
 test -n "$J11" || export J11="$(jhome '11-oracle')"
 test -n "$J12" || export J12="$(jhome '12-oracle')"
+test -n "$J13" || export J13="$(jhome '13-oracle')"
+test -n "$J14" || export J14="$(jhome '14-oracle')"
+test -n "$J15" || export J15="$(jhome '15-oracle')"
+test -n "$J16" || export J16="$(jhome '16-oracle')"
+test -n "$J17" || export J17="$(jhome '17-oracle')"
+test -n "$J18" || export J18="$(jhome '18-oracle')"
+test -n "$J19" || export J19="$(jhome '19-oracle')"
 alias j6='export JAVA_HOME="$J6" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j7='export JAVA_HOME="$J7" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j8='export JAVA_HOME="$J8" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
@@ -38,6 +52,13 @@ alias j9='export JAVA_HOME="$J9" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" &&
 alias j10='export JAVA_HOME="$J10" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j11='export JAVA_HOME="$J11" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 alias j12='export JAVA_HOME="$J12" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+alias j13='export JAVA_HOME="$J13" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+alias j14='export JAVA_HOME="$J14" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+alias j15='export JAVA_HOME="$J15" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+alias j16='export JAVA_HOME="$J16" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+alias j17='export JAVA_HOME="$J17" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+alias j18='export JAVA_HOME="$J18" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
+alias j19='export JAVA_HOME="$J19" && jswitch && echo "JAVA_HOME -> $JAVA_HOME" && java -version'
 
 # use Java 8 by default if available
 test -n "$J8" && export JAVA_HOME="$J8"
