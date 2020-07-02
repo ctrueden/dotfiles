7d52bbf9818fac51cf3a00e69012388a7c36ab08
Curtis Rueden <ctrueden@wisc.edu>
2020-07-01 23:13:11 -0500

    Update SSH shortcut aliases

--- plugins/machines.sh
+++ plugins/machines.sh
@@ -1,5 +1,5 @@
-alias sha='ssh loci.wisc.edu'             # abyssinian
+alias sha='ssh imagej.net'                # abyssinian
 alias shb='ssh maven.scijava.org'         # balinese
-alias shc='ssh dev.loci.wisc.edu'         # cheetoh
+alias shc='ssh 144.92.48.185'             # cheetoh
 alias shd='ssh 144.92.48.190'             # devonrex
 alias sho='ssh open.microscopy.wisc.edu'  # open
