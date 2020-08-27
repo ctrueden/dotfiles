6fca0b3baf205738cb4c9608f323f63ab8e7f4f5
Curtis Rueden <ctrueden@wisc.edu>
2020-08-26 22:54:06 -0500

    Prepend Ruby stuff from ~/.gem

--- plugins/zzz_path.sh
+++ plugins/zzz_path.sh
@@ -13,3 +13,9 @@ do
 		export PATH="$BREW/$dir:$PATH"
 	fi
 done
+
+# prepend local Ruby bin directory to the path, if applicable
+localGemDir=$(gem env 2>/dev/null | grep 'USER INSTALLATION DIRECTORY' | sed 's/.*: //')
+if [ -d "$localGemDir" -a -d "$localGemDir/bin" ]; then
+	export PATH="$localGemDir/bin:$PATH"
+fi
