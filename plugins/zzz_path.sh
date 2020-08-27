945dd481eb955ccaa6fdfaf93b5f57b0ea1a9a67
Curtis Rueden <ctrueden@wisc.edu>
2020-08-26 21:58:21 -0500

    Improve Homebrew path prepending logic
    And prepend Homebrew Ruby if it's installed.

--- plugins/zzz_path.sh
+++ plugins/zzz_path.sh
@@ -7,9 +7,9 @@ $CODE_FIJI/bin:\
 $PATH
 
 # prepend Homebrew bin directories to the path, if applicable
-if [ -d "$BREW/sbin" ]; then
-	export PATH="$BREW/sbin:$PATH"
-fi
-if [ -d "$BREW/bin" ]; then
-	export PATH="$BREW/bin:$PATH"
-fi
+for dir in bin sbin opt/ruby/bin
+do
+	if [ -d "$BREW/$dir" ]; then
+		export PATH="$BREW/$dir:$PATH"
+	fi
+done
