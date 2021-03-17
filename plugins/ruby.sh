51dc8c6673154f28c3821cec5f2c54bb528085f5
Curtis Rueden <ctrueden@wisc.edu>
2021-03-17 11:40:16 -0500

    Add Ruby config to make bundler work in user space

---
+++ plugins/ruby.sh
@@ -0,0 +1,13 @@
+# --== Ruby ==--
+
+# I know I "should" be using rbenv or RVM... but I already use one
+# virtual environment management tool, conda, and don't want to add
+# another Ruby-specific one to the mix.
+#
+# I'm happy with system Ruby for now, and the config below makes
+# bundler install gems without root access, rather than system-wide.
+
+if which ruby >/dev/null 2>&1
+then
+	export GEM_HOME=$(ruby -e 'puts Gem.user_dir')
+fi
