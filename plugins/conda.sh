41d365d07b10bf18a21c8cff9a967eea379e4d0f
Curtis Rueden <ctrueden@wisc.edu>
2021-09-21 12:42:05 -0500

    Prefer mamba to conda where feasible

--- plugins/conda.sh
+++ plugins/conda.sh
@@ -6,3 +6,10 @@ elif [ -d /usr/local/miniconda3 ]
 then
   . /usr/local/miniconda3/etc/profile.d/conda.sh
 fi
+
+# Prefer mamba to conda where feasible, if mamba is installed.
+if which mamba >/dev/null 2>&1
+then
+  export MAMBA_EXE=$(which mamba)
+  export CONDA_EXE=$MAMBA_EXE
+fi
