cd2895c7855647a654961642939c48ad85a59f57
Curtis Rueden <ctrueden@wisc.edu>
2021-09-21 15:43:10 -0500

    Rename 000_shell plugin to 000_init
    It's going to become more general, housing common functions.

--- plugins/000_init.sh
+++ plugins/000_init.sh
@@ -1,4 +1,4 @@
-test "$DEBUG" && echo "[dotfiles] Loading plugin 'shell'..."
+test "$DEBUG" && echo "[dotfiles] Loading plugin 'init'..."
 
 # Which shell am I running? zsh? bash?
 shell_name() { ps -p $$ | tail -n1 | sed 's/.* //'; }
