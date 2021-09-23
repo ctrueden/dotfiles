4b4eccf7ae502c7aa039c926f3aafe9d5625203b
Curtis Rueden <ctrueden@wisc.edu>
2021-09-23 12:31:10 -0500

    plugins/conda: cache conda init script
    Generating it is slow, and it's the same every time.

--- plugins/conda.sh
+++ plugins/conda.sh
@@ -1,17 +1,30 @@
 test "$DEBUG" && echo "[dotfiles] Loading plugin 'conda'..."
 
-for condaDir in "$HOME/miniconda3" /usr/local/miniconda
-do
-	# Adapted from "conda init" blurb.
-	__conda_setup="$("$condaDir/bin/conda" "shell.$(shell_name)" hook 2>/dev/null)"
-	if [ $? -eq 0 ]; then
-		eval "$__conda_setup"
-	elif [ -f "$condaDir/etc/profile.d/conda.sh" ]; then
-		. "$condaDir/etc/profile.d/conda.sh"
-	fi
-	unset __conda_setup
-	test -x "$CONDA_EXE" && break
-done
+condaSetupScript="$HOME/.cache/conda-setup"
+
+# Adapted from "conda init" blurb, but cached for performance.
+if [ ! -f "$condaSetupScript" ]
+then
+	for condaDir in "$HOME/miniconda3" /usr/local/miniconda
+	do
+		__conda_setup="$("$condaDir/bin/conda" "shell.$(shell_name)" hook 2>/dev/null)"
+		if [ $? -eq 0 ]; then
+			mkdir -p "$(dirname "$condaSetupScript")"
+			echo "$__conda_setup" >"$condaSetupScript"
+		elif [ -f "$condaDir/etc/profile.d/conda.sh" ]; then
+			mkdir -p "$(dirname "$condaSetupScript")"
+			echo ". '$condaDir/etc/profile.d/conda.sh'" >"$condaSetupScript"
+		fi
+		unset __conda_setup
+		test -f "$condaSetupScript" && break
+	done
+fi
+
+# Enable conda.
+if [ -f "$condaSetupScript" ]; then
+	. "$condaSetupScript"
+fi
+unset condaSetupScript
 
 # Enable mamba if available.
 if [ -f "$CONDA_PREFIX/etc/profile.d/mamba.sh" ]; then
