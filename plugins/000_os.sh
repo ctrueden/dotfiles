109db75fc1192a2f27047a8925a5995f58f9229d
Curtis Rueden <ctrueden@wisc.edu>
2021-12-18 16:18:55 -0600

    plugins/000_os: fix bug with open alias in bash

--- plugins/000_os.sh
+++ plugins/000_os.sh
@@ -32,7 +32,7 @@ fi
 
 # open a file with the default application using 'open'
 if [ "$IS_MACOS" ]; then
-	# The 'open' program is built in on macOS
+	: The 'open' program is built-in on macOS
 elif [ "$IS_LINUX" ]; then
 	alias open='xdg-open'
 elif [ "$IS_WINDOWS" ]; then
