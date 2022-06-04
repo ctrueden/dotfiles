c31527fdb9a761056ac73f635db5dc9a58da940e
Curtis Rueden <ctrueden@wisc.edu>
2022-06-04 16:20:31 -0500

    Switch to mambaforge
    https://github.com/conda-forge/miniforge#mambaforge
    
    Configuration now lives directly in .zshrc, managed by conda/mamba.
    
    Initialization seems much faster than it used to be,
    so my caching hacks are hopefully obsolete now.

---
+++ plugins/mamba.sh
@@ -0,0 +1,6 @@
+test "$DEBUG" && echo "[dotfiles] Loading plugin 'mamba'..."
+
+# Add handy shortcuts for efficient mamba usage.
+alias snk=mamba
+alias snka='mamba activate'
+alias snkd='mamba deactivate'
