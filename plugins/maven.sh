da70e580a2c932b27c9bb9bf16c3b1cf548115ca
Curtis Rueden <ctrueden@wisc.edu>
2022-07-01 16:16:43 -0500

    Make the lic command smarter
    If the current directory is not a Maven project
    with a supported license, abort accordingly.

--- plugins/maven.sh
+++ plugins/maven.sh
@@ -8,7 +8,20 @@ if [ -d "$BREW/Cellar/maven2" ]; then
 fi
 
 # license-maven-plugin
-alias lic='mvn license:update-project-license license:update-file-header'
+lic() {
+	local quiet=
+	test "$1" = "-q" && quiet=true
+	test -f pom.xml || {
+		test -z "$quiet" && echo "[ERROR] Not a Maven project."
+		return 1
+	}
+	local licName=$(grep 'license\.licenseName' pom.xml | sed -e 's/[^>]*>//' -e 's/<.*//')
+	test "$licName" -a "$licName" != "N/A" || {
+		test -z "$quiet" && echo "[ERROR] Unsupported license: $licName"
+		return 2
+	}
+	mvn license:update-file-header license:update-project-license
+}
 
 # dependency-maven-plugin
 alias anal='mvn dependency:analyze | grep WARNING'
