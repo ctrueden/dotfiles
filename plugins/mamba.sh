e6dfc3bc0536ce61858b311b29b249d89ed73942
Curtis Rueden <ctrueden@wisc.edu>
2023-01-03 15:11:12 -0600

    plugins/mamba: forbid installation into base

--- plugins/mamba.sh
+++ plugins/mamba.sh
@@ -1,7 +1,17 @@
 test "$DEBUG" && echo "[dotfiles] Loading plugin 'mamba'..."
 
+snk-base-check() {
+  # NB: Fail fast if we are in the base environment!
+  # Otherwise, it's too easy to install packages by accident into base.
+  if [ "$CONDA_PREFIX" -a "$CONDA_PREFIX" != "${CONDA_PREFIX%/base}" ]
+  then
+    echo "Aborting because you are in the base environment. Activate something else."
+    return 1
+  fi
+}
+
 # Add handy shortcuts for efficient mamba usage.
-alias snk=mamba
+alias snk='snk-base-check && mamba'
 alias snka='mamba activate'
 alias snkd='mamba deactivate'
 alias snke='mamba env'
