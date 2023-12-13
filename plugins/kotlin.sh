dda7df9b35e19c1c81c220c7b685493167791b3f
Curtis Rueden <ctrueden@wisc.edu>
2023-12-13 10:16:50 -0600

    plugins/kotlin: make it easier to call konanc

--- plugins/kotlin.sh
+++ plugins/kotlin.sh
@@ -2,3 +2,17 @@ test "$DEBUG" && echo "[dotfiles] Loading plugin 'kotlin'..."
 
 alias ktc=kotlinc
 alias kts=kscript
+
+# Make konanc and friends available on the CLI. Kotlin downloads them
+# on demand and squirrels them away, so let's find the newest one.
+newestKonanDir=$(for d in ~/.konan/kotlin-native-prebuilt-*/
+do
+  v=${d%*/}
+  v=${v##*-}
+  echo "$v|$d"
+done | sort -nr | head -n1 2>/dev/null)
+export KONAN_DIR=${newestKonanDir#*|}
+if [ -d "$KONAN_DIR" ]
+then
+  path_prepend "$KONAN_DIR/bin"
+fi
