d61478b03746abe9e7e8ee1a2f3f31f48fc23d4c
Curtis Rueden <ctrueden@wisc.edu>
2025-04-18 12:05:17 -0500

    Let the python plugin install uv

--- plugins/python.sh
+++ plugins/python.sh
@@ -1,3 +1,17 @@
 test "$DEBUG" && echo "[dotfiles] Loading plugin 'python'..."
 
 pypath() { python -c 'import sys; print("\n".join(sys.path))'; }
+
+# --== uv ==--
+
+if [ ! -x "$(command -v uv)" ]; then
+  # Install uv via pip if possible.
+  if [ -x "$(command -v pip)" ]; then
+    pip install --user uv
+  fi
+fi
+
+if [ -x "$(command -v uv)" ]; then
+  # Enable uv shell completion.
+  eval "$(uv generate-shell-completion zsh)"
+fi
