0a1296dd9e9bb202e9d4e182c225bfdc6b15a302
Curtis Rueden <ctrueden@wisc.edu>
2025-04-23 15:42:57 -0500

    Improve uve shortcut to install the tool editably
    I didn't know about the --with-editable flag. Better!

--- plugins/python.sh
+++ plugins/python.sh
@@ -21,19 +21,19 @@ if [ -x "$(command -v uv)" ]; then
   alias uvl='uv tool list'
   uve() {
 : << 'DOC'
-Install the Python project in the current working directory as a uv tool,
-replacing any previous installation. As usual with `uv tool install`,
+Install the Python project in the current working directory as an editable
+tool, replacing any previous installation. As usual with `uv tool install`,
 there will be a symlink in `~/.local/bin` pointing to
 `~/.local/share/uv/tools/<pkgname>/bin/<exename>`, but in this case, the
-tool's source is copied from the local computer, not from the Internet.
+tool's source is linked from a local working copy, not downloaded or copied.
 
 Intended as an approximate replacement for `pip install -e . --user`,
-although the installed tool is not hotlinked, so ignores future code changes.
+although the hotlinked tool is installed into its own isolated environment.
 DOC
     test -e pyproject.toml || { >&2 echo '[ERROR] No pyproject.toml.'; exit 1; }
     proj=$(grep '^[ \t]*name[ \t]*=' pyproject.toml |
       sed "s/name[ \t]*=[ \t]*[\"']\([^\"']*\).*/\1/")
     test "$proj" || { >&2 echo '[ERROR] Cannot discern project name.'; exit 2; }
-    uv tool install --reinstall-package "$proj" --from . "$proj"
+    uv tool install --reinstall-package "$proj" --with-editable . "$proj"
   }
 fi
