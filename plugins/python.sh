dcc161b00f9c832afebaabae01244fce5a33d605
Curtis Rueden <ctrueden@wisc.edu>
2025-04-23 14:06:58 -0500

    Add uv convenience shorthands

--- plugins/python.sh
+++ plugins/python.sh
@@ -14,4 +14,26 @@ fi
 if [ -x "$(command -v uv)" ]; then
   # Enable uv shell completion.
   eval "$(uv generate-shell-completion zsh)"
+
+  # Add uv convenience shorthands.
+  alias uvi='uv tool install'
+  alias uvu='uv tool uninstall'
+  alias uvl='uv tool list'
+  uve() {
+: << 'DOC'
+Install the Python project in the current working directory as a uv tool,
+replacing any previous installation. As usual with `uv tool install`,
+there will be a symlink in `~/.local/bin` pointing to
+`~/.local/share/uv/tools/<pkgname>/bin/<exename>`, but in this case, the
+tool's source is copied from the local computer, not from the Internet.
+
+Intended as an approximate replacement for `pip install -e . --user`,
+although the installed tool is not hotlinked, so ignores future code changes.
+DOC
+    test -e pyproject.toml || { >&2 echo '[ERROR] No pyproject.toml.'; exit 1; }
+    proj=$(grep '^[ \t]*name[ \t]*=' pyproject.toml |
+      sed "s/name[ \t]*=[ \t]*[\"']\([^\"']*\).*/\1/")
+    test "$proj" || { >&2 echo '[ERROR] Cannot discern project name.'; exit 2; }
+    uv tool install --reinstall-package "$proj" --from . "$proj"
+  }
 fi
