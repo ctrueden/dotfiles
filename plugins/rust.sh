be3805abbabcbb7b1500f165fe4be27450519624
Curtis Rueden <ctrueden@wisc.edu>
2025-05-18 16:48:18 -0500

    Add rust config

--- plugins/rust.sh
+++ plugins/rust.sh
@@ -1,10 +1,5 @@
 test "$DEBUG" && echo "[dotfiles] Loading plugin 'rust'..."
 
-# If cargo is installed, add its bin directory to the system path.
-if [ -d "$CARGO_HOME" ]
-then
-  path_prepend "$CARGO_HOME/bin"
-elif [ -d "$HOME/.cargo" ]
-then
-  path_prepend "$HOME/.cargo/bin"
+if [ -f "$HOME/.cargo/env" ]; then
+  . "$HOME/.cargo/env"
 fi
