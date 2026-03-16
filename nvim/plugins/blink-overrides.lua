6176b107cfc7f65a2121f2631bf53baa2934cecc
Curtis Rueden <ctrueden@wisc.edu>
2026-03-16 11:22:54 -0500

    nvim: only pop the autocomplete upon ctrl+space
    Otherwise, when typing words, it's constantly flashing
    all over the place and words get autocorrupted sometimes.

--- nvim/plugins/blink-overrides.lua
+++ nvim/plugins/blink-overrides.lua
@@ -5,7 +5,14 @@ return {
       preset = 'default',
       ['<Tab>'] = { 'accept', 'fallback' },
       ['<C-.>'] = { 'show', 'accept', 'fallback' },
+      ['<C-Space>'] = { 'show', 'fallback' },
       ['<CR>'] = { 'accept', 'fallback' },
+    },
+    completion = {
+      trigger = {
+        show_on_insert_on_trigger_character = false,
+        show_on_keyword = false,
+      },
     },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
