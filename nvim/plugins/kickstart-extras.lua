949383f1833e468b389cb29531140f38a3ae91e1
Curtis Rueden <ctrueden@wisc.edu>
2026-07-17 12:55:29 -0500

    nvim: reenable nvim-dap plugin
    So that nvim can be used as a debugger tool.
    And some day I'll actually figure out how to use it!

--- nvim/plugins/kickstart-extras.lua
+++ nvim/plugins/kickstart-extras.lua
@@ -1,8 +1,8 @@
 -- Enable opt-in kickstart.nvim plugins without modifying init.lua directly.
 -- Other available kickstart plugins include:
---   debug    - nvim-dap debugger
 --   lint     - nvim-lint linter
 --   autopairs - auto-close brackets
 --   neo-tree - file explorer sidebar
 --   gitsigns - git hunk signs + keymaps
+require 'kickstart.plugins.debug' -- nvim-dap debugger
 require 'kickstart.plugins.indent_line'
